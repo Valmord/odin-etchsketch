e4b00d40946d4c8ffdeb7ef238c000b2c34c1f0b
TC <nothing@nothing.com>
2023-12-23 11:44:52 +1300

    Created show grid button and added button formatting

--- script/code.js
+++ script/code.js
@@ -1,11 +1,12 @@
 //Create Grid
 let gridSize = 16;
+let gridToggled = false;
 renderGrid();
-
 
 function renderGrid(){
     drawGrid();
     addSquareListeners();
+    if (gridToggled) toggleGridLines();
 }
 
 function drawGrid(){
@@ -31,12 +32,31 @@ function addSquareListeners(){
     })
 }
 
+function toggleGridLines(){
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach(square => square.classList.toggle('square-borders'));
+}
+
 const gridSizeButton = document.querySelector('.grid-size-js')
 gridSizeButton.addEventListener('click', () => {
-    let userGridChoice = +prompt('Please enter a whole number between 1 and 100', 16)
-    while(isNaN(userGridChoice) || userGridChoice > 100 || userGridChoice < 1){
-        userGridChoice = +prompt('Please enter a number between 1 and 100!');
-    }
-    gridSize = Math.floor(userGridChoice);
-    renderGrid();
+    gridSizeButton.classList.toggle('toggled-button');
+
+    // Timeout added so you can see bugged is toggled for duration of prompt
+    setTimeout( () => {
+        let userGridChoice = +prompt('Please enter a whole number between 1 and 100', 16)
+        while(isNaN(userGridChoice) || userGridChoice > 100 || userGridChoice < 1){
+            userGridChoice = +prompt('Please enter a number between 1 and 100!');
+        }
+        gridSize = Math.floor(userGridChoice);
+        renderGrid();
+        gridSizeButton.classList.toggle('toggled-button');
+    }, 5)
+
+})
+
+const toggleGridButton = document.querySelector('.toggle-grid')
+toggleGridButton.addEventListener('click',() => {
+    toggleGridLines();
+    gridToggled = !gridToggled;
+    toggleGridButton.classList.toggle('toggled-button');
 })
