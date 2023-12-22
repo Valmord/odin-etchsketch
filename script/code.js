0d73535f712f3cb70dc786e8539977c48aca0a0a
TC <nothing@nothing.com>
2023-12-23 12:34:13 +1300

    Rainbow button and toggle added

--- script/code.js
+++ script/code.js
@@ -1,21 +1,39 @@
 //Create Grid
-let gridSize = 16;
-let gridToggled = false;
+const settings = {
+    gridSize: 16,
+    gridToggled: false,
+    rainbow: false,
+    grayScale: false,
+    colorMode: true,
+    color: 'red',
+
+    changeMode(mode){
+        if (this[mode]) {
+            [this.rainbow, this.grayScale, this.colorMode,] = 
+            [false, false, true]
+        } else {
+            [this.rainbow, this.grayScale, this.colorMode,] = 
+            [false, false, false]
+            this[mode] = !this[mode]
+        }
+    }
+}
+
 renderGrid();
 
 function renderGrid(){
     drawGrid();
     addSquareListeners();
-    if (gridToggled) toggleGridLines();
+    if (settings.gridToggled) toggleGridLines();
 }
 
 function drawGrid(){
     const gridContainer = document.querySelector('.grid-container');
     let gridHtml = '';
-    for (let i = 0; i < gridSize; i++){
+    for (let i = 0; i < settings.gridSize; i++){
         gridHtml += `<div class='square-container grid-${i}'>`
         
-        for (let j = 0; j < gridSize; j++){
+        for (let j = 0; j < settings.gridSize; j++){
             gridHtml += `\n<div class="grid-square"></div>`;
         }
         gridHtml += `</div>\n`   
@@ -27,15 +45,35 @@ function addSquareListeners(){
     const gridSquares = document.querySelectorAll('.grid-square')
     gridSquares.forEach( node => {
         node.addEventListener('mouseenter', () => {
-            node.classList.add('sketched')
+            setColour(node);
         })
     })
 }
 
-function toggleGridLines(){
-    const gridSquares = document.querySelectorAll('.grid-square');
-    gridSquares.forEach(square => square.classList.toggle('square-borders'));
+function setColour(node){
+    if (settings.rainbow){
+        node.style['background-color'] = getRandomHexColour();
+    } else if (settings.grayScale) {
+
+    } else if (settings.colorMode) {
+        node.style['background-color'] = settings.color;
+    }
 }
+
+const rainbowButton = document.querySelector('.rainbow-button')
+rainbowButton.addEventListener('click',()=>{
+    settings.changeMode('rainbow');
+    rainbowButton.classList.toggle('toggled-button');
+})
+
+function toggleRainbowMode(){
+
+}
+
+function getRandomHexColour(){
+    return '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+}
+
 
 const gridSizeButton = document.querySelector('.grid-size-js')
 gridSizeButton.addEventListener('click', () => {
@@ -54,9 +92,15 @@ gridSizeButton.addEventListener('click', () => {
 
 })
 
+
+function toggleGridLines(){
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach(square => square.classList.toggle('square-borders'));
+}
+
 const toggleGridButton = document.querySelector('.toggle-grid')
 toggleGridButton.addEventListener('click',() => {
     toggleGridLines();
-    gridToggled = !gridToggled;
+    settings.gridToggled = !settings.gridToggled;
     toggleGridButton.classList.toggle('toggled-button');
 })
